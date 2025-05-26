82d669d2bd51c29276db9c4d81608abec6b524be
Roger Lu <tariffalert.me@gmail.com>
2025-05-26 00:15:02 -0700

    chore: ensure Google AdSense script is present in <head> on every page

--- app/layout.tsx
+++ app/layout.tsx
@@ -21,12 +21,8 @@ export default function RootLayout({
   return (
     <html lang="en">
       <head>
-        {/* Google AdSense Verification */}
-        <script
-          async
-          src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-3704612965635562"
-          crossOrigin="anonymous"
-        ></script>
+        {/* Google AdSense */}
+        <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-3704612965635562" crossOrigin="anonymous"></script>
         {/* Google Analytics (gtag.js) */}
         <script async src="https://www.googletagmanager.com/gtag/js?id=G-2KD9CZD16K"></script>
         <script dangerouslySetInnerHTML={{ __html: `
